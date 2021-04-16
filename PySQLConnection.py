0859e969ce2d9a07c0e077768583fe4beee27cd9
divyavenkatraman <44758649+divyavenkatraman@users.noreply.github.com>
2021-04-16 12:13:24 -0400

    searching works

--- PySQLConnection.py
+++ PySQLConnection.py
@@ -1,7 +1,29 @@
-import queries
+
 import cryptography
 import pymysql
+import importlib
 
+def search(cursor, dialogue, allCols, table, onCol):
+    x = input(dialogue + "\n")
+    query = "select " + allCols + " from " + table + " where " + onCol + " like '" + x + "%'"
+    while not (cursor.execute(query) == 1):
+        print("Not an exact match. Did you mean one of these?")
+        for i in cursor:
+            print(i)
+        x = input(dialogue + "\n")
+        query = "select " + allCols + " from " + table + " where " + onCol + " like '" + x + "%'"
+    for i in cursor:
+        return i
+
+
+
+#Searches for ingredient by name, returns nutritonal data
+def find_ingredient(cursor):
+    x = search(cursor, "Enter a food item", "food_id, food_name", "food_item", "food_name")
+    return x[0]
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------i
 print('Hi!')
 connection = pymysql.connect(host='localhost',
                              port=3306,
